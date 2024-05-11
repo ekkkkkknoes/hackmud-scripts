526eb5b59f7574754bcab2e389719c077459542a
Ekkie <soelam@live.de>
2024-05-11 11:42:33 +0200

    Add last few t2 locks

--- crackt2.js
+++ crackt2.js
@@ -8,35 +8,114 @@ function (ctx,a) { // t:#s.some.npc
     l,
     k,
     b,
+    i,
+    j,
+    s,
+    tx,
     out = [],
+    lib = #fs.scripts.lib(),
     log = x => {
         out.push(`[\`2${(Date.now() - _START + "").padStart(4)}\`] ` + x)
         return out
     },
-    f = (k, v) => log(`Solved \`N${k}\`: \`V${v}\``),
-    lib = #fs.scripts.lib()
+    f = (k, v) => log(`Solved \`N${k}\`: \`V${v}\``)
     log(`Cracking ${a.t.name}`)
     while (!/nection terminated.$/.exec(r)) {
         if (_END - Date.now() < 500)
             return {ok: !1, msg: [...log("`DHit timeout!`"), r, p]}
-        if (!((l = /`N(.*)` (lock.|is missing.)$/.exec(r)) && (l = l[1])))
+        if (/To unlock, please load the appropriate k3y:/.test(r))
+            l = "l0ckbox"
+        else if (!((l = /`N(.*)` (lock.|is missing.)$/.exec(r)) && (l = l[1])))
             return {ok: !1, msg: [...log("`DCouldn't find key`"), r, p]}
         log(`Found \`N${l}\``)
         switch (l) {
+        case "l0ckbox":
+            k = r.split` `.pop()
+            #ls.matr1x.r3dbox({return: true})
+            s = #hs.sys.upgrades({full:true}).filter(x => "k3y" in x)
+            #ms.sys.manage({unload: s.map(x=>x.i)})
+            if (!s.find(x => x.k3y == k))
+                #ls.matr1x.r3dbox({request: k})
+            b = #hs.sys.upgrades({full:true}).find(x => x.k3y == k)
+            if (b) {
+                #ms.sys.manage({load: b.i})
+                r = c()
+                f(l, k)
+                break
+            }
+            return {ok:!1, msg: [...log(`\`DDon't have k3y ${k}\``), r, p]}
         case "acct_nt":
             p[l] = ""
-            return {ok: !1, msg: [...log("`DCan't solve acct_nt`"), #hs.accts.transactions({count: 25}).map(x=>lib.to_game_timestr(x.time) + `: \`${x.recipient == ctx.caller ? "L" : "D"}${x.amount}\` "${x.memo || ""}"`), c(), p]}
+            r = c()
+            tx = #hs.accts.transactions({count: 35})
+            tx.forEach(x => x.time = lib.to_game_timestr(x.time))
+            s = []
+            if (b = /^What was the net GC between (.+) and (.+)$/.exec(r)){
+                tx = tx.filter(x => x.time >= b[1] && x.time <= b[2])
+                for (i = 0; i <= tx.filter(x => x.time == b[2]).length; i++)
+                    for (j = 0; j <= tx.filter(x => x.time == b[1]).length; j++)
+                        s.push(tx.slice(i, tx.length - j).reduce((a, x) => a + x.amount * (x.sender == ctx.caller ? -1 : 1), 0))
+                s = s.filter((v, i, a) => a.indexOf(v) == i)
+                for (k of s) {
+                    p[l] = k
+                    if ((r = c()) != b[0]) break
+                }
+            } else if (b = /^Need to know the total (earned|spent) on transactions (with|without) memos between (.+) and (.+)$/.exec(r)) {
+                tx = tx.filter(x =>
+                    b[1] == "spent" ? x.sender == ctx.caller : x.sender != ctx.caller &&
+                    b[2] == "with" ? "memo" in x : !("memo" in x) &&
+                    x.time >= b[3] &&
+                    x.time <= b[4])
+                for (i = 0; i <= tx.filter(x => x.time == b[4]).length; i++)
+                    for (j = 0; j <= tx.filter(x => x.time == b[3]).length; j++)
+                        s.push(tx.slice(i, tx.length - j).reduce((a, x) => a + x.amount * (x.sender == ctx.caller ? -1 : 1), 0))
+                s = s.filter((v, i, a) => a.indexOf(v) == i)
+                for (k of s) {
+                    p[l] = k
+                    if ((r = c()) != b[0]) break
+                }
+            } else if (b = /^Get me the amount of a large (deposit|withdrawal) near (.+)$/.exec(r)) {
+                tx = tx.filter(x => b[1] == "deposit" ? x.sender != ctx.caller : x.sender == ctx.caller)
+                for (k of tx) {
+                    p[l] = k.amount
+                    if ((r = c()) != b[0]) break
+                }
+            }
+            if (r != b[0])
+                f(l, p[l])
+            else
+                return {ok: !1, msg: [...log("`DCan't solve acct_nt`"), #hs.accts.transactions({count: 35}).map(x=>lib.to_game_timestr(x.time) + `: \`${x.recipient == ctx.caller ? "L" : "D"}${x.amount}\` "${x.memo || ""}"`), r, p]}
+            break
         case "CON_SPEC":
             p[l] = ""
-            return {ok: !1, msg: [...log("`DCan't solve CON_SPEC`"), c(), p]}
+            k = (r = c()).split`\n`
+            if (k[1] != "Provide the next three letters in the sequence")
+                return {ok: !1, msg: [...log("`DCan't solve this CON_SPEC`"), r, p]}
+            k = k[0].split``.map(x=>x.charCodeAt())
+            p[l] = String.fromCharCode(...[3,4,5].map(x=>k[k.length-1] + k[x] - k[2]))
+            r = c()
+            f(l, p[l])
+            break
         case "sn_w_glock":
-            b = #ms.shia.labank({d:1})
-            if (!b.ok)
-                return {ok: !1, msg: [...log('`DBanking error`'), b.msg, r, p]}
+            if (#hs.accts.balance() > 0) {
+                b = #ms.shia.labank({d:1})
+                if (!b.ok)
+                    return {ok: !1, msg: [...log('`DBanking error`'), b.msg, r, p]}
+                if ("acct_nt" in p) {
+                    log("Resetting acct_nt")
+                    delete p.acct_nt
+                    r = c()
+                    break
+                }
+            }
             p[l] = ""
             b = #ms.shia.labank({l: (r = c())})
             if (!b.ok)
                 return {ok: !1, msg: [...log('`DBanking error`'), b.msg, r, p]}
+            if ("acct_nt" in p) {
+                log("Resetting acct_nt")
+                delete p.acct_nt
+            }
             r = c()
             log(`Solved \`N${l}\` (hopefully, I'm not checking)`)
             break
